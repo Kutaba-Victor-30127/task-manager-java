2a2dbab0ee1a5d1c1889ad476405d28b7f0b62a4
Kutaba Victor <victor.kutaba@gmail.com>
2026-06-04 17:06:14 +0300

    fix CorsConfig

--- backend/src/main/java/ro/kutaba/taskmanager/config/CorsConfig.java
+++ backend/src/main/java/ro/kutaba/taskmanager/config/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://127.0.0.1:5500",
-                                "http://localhost:5500"
+                                "http://localhost:5500",
+                                "https://gorgeous-churros-7fff2c.netlify.app"
                         )  
                         .allowedMethods("*")
                         .allowedHeaders("*")
